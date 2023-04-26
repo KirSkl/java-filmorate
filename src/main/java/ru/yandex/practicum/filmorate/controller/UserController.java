ccbaa6d5daa8ff8595d0d363f0747cab4e9a7fa3
Kirill Skladanov <wra1th@inbox.ru>
2023-04-26 15:32:06 +0300

    <feat> добавлено логирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -14,6 +16,7 @@ import java.util.Map;
 public class UserController {
 
     private final Map<Integer, User> users = new HashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping
     public Collection<User> findAllUsers() {
@@ -22,23 +25,29 @@ public class UserController {
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
         if (user.getLogin().contains(" ")) {
+            log.info("Ошибочный запрос POST /users - логин содержит пробелы");
             throw new ValidationException("Логин не должен содержать пробелов");
         }
         if (user.getName().isBlank()) {
              user.setName(user.getLogin());
         }
+        log.info("Получен запрос POST /users - добавление поьзователя");
         users.put(user.getId(), user);
+        log.info("Пользователь добавлен");
         return user;
     }
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) {
         if (user.getLogin().contains(" ")) {
+            log.info("Ошибочный запрос PUT /users - логин содержит пробелы");
             throw new ValidationException("Логин не должен содержать пробелов");
         }
         if (user.getName().isBlank()) {
             user.setName(user.getLogin());
         }
+        log.info("Получен запрос PUT /users - обновление пользователя");
         users.put(user.getId(), user);
+        log.info("Пользователь обновлен");
         return user;
     }
 }
