8b4de0de0341f9b084b22effbd654eea7519fdcc
Kirill Skladanov <wra1th@inbox.ru>
2023-04-26 15:27:20 +0300

    <feat> добавлено логирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -13,6 +15,7 @@ import java.util.*;
 public class FilmController {
 
     public final Map<Integer, Film> films = new HashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(FilmController.class);
 
     @GetMapping
     public Collection<Film> findAllFilms() {
@@ -21,23 +24,29 @@ public class FilmController {
     @PostMapping
     public Film createFilm(@Valid @RequestBody Film film) {
         if (film.getDuration().isNegative() || film.getDuration().isZero()) {
+            log.info("Ошибочный запрос POST /films - указана не положительная продолжительность");
             throw new ValidationException("Продолжительность должна быть положительной");
         }
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.info("Ошибочный запрос POST /films - указана слишком ранняя дата релиза");
             throw new ValidationException("дата релиза должна быть не раньше 28 декабря 1895 года");
         }
+        log.info("Получен запрос POST /films - добавление фильма");
         films.put(film.getId(), film);
         return film;
     }
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film film) {
         if (film.getDuration().isNegative() || film.getDuration().isZero()) {
+            log.info("Ошибочный запрос POST /films - указана не положительная продолжительность");
             throw new ValidationException("Продолжительность должна быть положительной");
         }
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.info("Ошибочный запрос POST /films - указана слишком ранняя дата релиза");
             throw new ValidationException("дата релиза должна быть не раньше 28 декабря 1895 года");
         }
         films.put(film.getId(), film);
+        log.info("Получен запрос PUT /films - обновление фильма");
         return film;
     }
 }
