4d198556aee70131e651843908b228e6ae1c817a
Kirill Skladanov <wra1th@inbox.ru>
2023-05-01 22:53:56 +0300

    <feat> вынести валидацию в отдельный класс, добавить получение ID

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,8 +3,10 @@ package ru.yandex.practicum.filmorate.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.util.Validator;
 
 import javax.validation.Valid;
 import java.util.Collection;
@@ -16,6 +18,7 @@ import java.util.Map;
 public class UserController {
 
     private final Map<Integer, User> users = new HashMap<>();
+    private int id = 0;
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping
@@ -24,30 +27,26 @@ public class UserController {
     }
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
-        if (user.getLogin().contains(" ")) {
-            log.info("Ошибочный запрос POST /users - логин содержит пробелы");
-            throw new ValidationException("Логин не должен содержать пробелов");
-        }
-        if (user.getName().isBlank()) {
-             user.setName(user.getLogin());
-        }
-        log.info("Получен запрос POST /users - добавление поьзователя");
+        log.info("Получен запрос POST /users - добавление пользователя");
+        Validator.validateUser(user);
+        user.setId(getId());
         users.put(user.getId(), user);
         log.info("Пользователь добавлен");
         return user;
     }
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) {
-        if (user.getLogin().contains(" ")) {
-            log.info("Ошибочный запрос PUT /users - логин содержит пробелы");
-            throw new ValidationException("Логин не должен содержать пробелов");
-        }
-        if (user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
         log.info("Получен запрос PUT /users - обновление пользователя");
+        if (!users.containsKey(user.getId())) {
+            log.info("Неверный запрос PUT /users - пользователь не найден");
+            throw new UserNotFoundException("Пользователь с таким ID не найден");
+        }
+        Validator.validateUser(user);
         users.put(user.getId(), user);
         log.info("Пользователь обновлен");
         return user;
+    }
+    private int getId() {
+        return ++id;
     }
 }
