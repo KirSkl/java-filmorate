b1dc103abc53f235d88ca283674b2611bc6b3619
Kirill Skladanov <wra1th@inbox.ru>
2023-05-01 22:49:30 +0300

    <feat> тип продолжительности изменен на int

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,16 +4,15 @@ import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.time.Duration;
 import java.time.LocalDate;
 
 @Data
 public class Film {
-    private final int id;
+    private int id;
     @NotBlank
     private final String name;
     @Size(max=200, message = "максимальная длина описания — 200 символов")
     private final String description;
     private final LocalDate releaseDate;
-    private final Duration duration;
+    private final int duration;
 }
