0dde84d328e7ad1866162bbebc35a20607ed61fd
Kirill Skladanov <wra1th@inbox.ru>
2023-05-03 15:50:05 +0300

    <fix> добавлены работающие конструкторы

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,16 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
-@RequiredArgsConstructor
 public class Film {
     private int id;
     @NotBlank
@@ -19,4 +17,23 @@ public class Film {
     private final String description;
     private final LocalDate releaseDate;
     private final int duration;
+
+    @JsonCreator
+    public Film(@JsonProperty int id, @JsonProperty String name, @JsonProperty String description,
+            @JsonProperty LocalDate releaseDate, @JsonProperty int duration) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+    }
+
+    @JsonCreator
+    public Film(@JsonProperty String name, @JsonProperty String description, @JsonProperty LocalDate releaseDate,
+            @JsonProperty int duration) {
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+    }
 }
