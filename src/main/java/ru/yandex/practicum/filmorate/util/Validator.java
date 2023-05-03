20e8c6a88d59d86d6fe90c34870b0b4e9e1924f7
Kirill Skladanov <wra1th@inbox.ru>
2023-05-03 12:11:05 +0300

    <fix> исправить тексты логов

--- src/main/java/ru/yandex/practicum/filmorate/util/Validator.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/Validator.java
@@ -14,17 +14,17 @@ public class Validator {
 
     public static void validateFilm(Film film) {
         if (film.getDuration() <=0) {
-            log.info("Ошибочный запрос POST /films - указана не положительная продолжительность");
+            log.info("Ошибочный запрос - указана не положительная продолжительность");
             throw new ValidationException("Продолжительность должна быть положительной");
         }
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            log.info("Ошибочный запрос POST /films - указана слишком ранняя дата релиза");
+            log.info("Ошибочный запрос - указана слишком ранняя дата релиза");
             throw new ValidationException("Дата релиза должна быть не раньше 28 декабря 1895 года");
         }
     }
     public static void validateUser(User user) {
         if (user.getLogin().contains(" ")) {
-            log.info("Ошибочный запрос POST /users - логин содержит пробелы");
+            log.info("Ошибочный запрос - логин содержит пробелы");
             throw new ValidationException("Логин не должен содержать пробелов");
         }
     }
