6ad677063c3a5a5d924935c014898370e7b9a9e5
Kirill Skladanov <wra1th@inbox.ru>
2023-05-17 16:04:54 +0300

    <feat> добавить объявление методов

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -1,4 +1,9 @@
 package ru.yandex.practicum.filmorate.storage;
 
+import ru.yandex.practicum.filmorate.model.User;
+
 public interface UserStorage {
+    User addUser(User user);
+    User updateUser(User user);
+    void removeUser(int id);
 }
