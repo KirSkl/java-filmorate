442898552e82a137b8ee7e4551c7ad67d1672693
Kirill Skladanov <wra1th@inbox.ru>
2023-05-20 14:49:40 +0300

    <feat> перенести логику хранения в класс storage

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,10 +2,12 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 import ru.yandex.practicum.filmorate.util.Validator;
 
 import javax.validation.Valid;
@@ -16,22 +18,24 @@ import java.util.Map;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
-    private final Map<Integer, User> users = new HashMap<>();
-    private int id = 0;
+    InMemoryUserStorage userStorage;
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    public UserController(InMemoryUserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
 
     @GetMapping
     public Collection<User> findAllUsers() {
-        return users.values();
+        return userStorage.getUsers().values();
     }
 
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
         log.info("Получен запрос POST /users - добавление пользователя");
         Validator.validateUser(user);
-        user.setId(getId());
-        users.put(user.getId(), user);
+        userStorage.addUser(user);
         log.info("Пользователь добавлен");
         return user;
     }
@@ -39,17 +43,18 @@ public class UserController {
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) {
         log.info("Получен запрос PUT /users - обновление пользователя");
-        if (!users.containsKey(user.getId())) {
-            log.info("Неверный запрос PUT /users - пользователь не найден");
-            throw new UserNotFoundException("Пользователь с таким ID не найден");
-        }
         Validator.validateUser(user);
-        users.put(user.getId(), user);
+        userStorage.updateUser(user);
         log.info("Пользователь обновлен");
         return user;
     }
 
-    private int getId() {
-        return ++id;
+    @DeleteMapping
+    public void removeUser(@RequestBody Long id) {
+        log.info("Получен запрос DELETE /users - удаление пользователя");
+        userStorage.removeUser(id);
+        log.info("Пользователь удален");
     }
+
+
 }
