30b8edc8ce0f034fd2251f142a96a57d96e5716a
Kirill Skladanov <wra1th@inbox.ru>
2023-05-20 14:48:32 +0300

    <feat> перенести логику хранения в класс storage

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,10 +2,13 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 import ru.yandex.practicum.filmorate.util.Validator;
 
 import javax.validation.Valid;
@@ -14,37 +17,35 @@ import java.util.*;
 @RestController
 @RequestMapping("/films")
 public class FilmController {
-
-    public final Map<Integer, Film> films = new HashMap<>();
-    private int id = 0;
+    static InMemoryFilmStorage filmStorage;
     private static final Logger log = LoggerFactory.getLogger(FilmController.class);
-
+    @Autowired
+    public FilmController(InMemoryFilmStorage filmStorage) {
+        this.filmStorage = filmStorage;
+    }
     @GetMapping
     public Collection<Film> findAllFilms() {
-        return films.values();
+        return filmStorage.getFilms().values();
     }
 
     @PostMapping
     public Film createFilm(@Valid @RequestBody Film film) {
-        Validator.validateFilm(film);
         log.info("Получен запрос POST /films - добавление фильма");
-        film.setId(getId());
-        films.put(film.getId(), film);
-        return film;
+        Validator.validateFilm(film);
+        return filmStorage.addFilm(film);
     }
 
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film film) {
-        if (!films.containsKey(film.getId())) {
-            throw new FilmNotFoundException("Фильм с таким ID не найден");
-        }
-        Validator.validateFilm(film);
-        films.put(film.getId(), film);
         log.info("Получен запрос PUT /films - обновление фильма");
-        return film;
+        Validator.validateFilm(film);
+        return filmStorage.updateFilm(film);
     }
 
-    private int getId() {
-        return ++id;
+    @DeleteMapping
+    public void removeFilm(@RequestBody Long id) {
+        log.info("Получен запрос DELETE /films - удаление фильма");
+        filmStorage.removeFilm(id);
+        log.info("Фильм удален");
     }
 }
