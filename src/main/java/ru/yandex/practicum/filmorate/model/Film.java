c85dbaf2cffdfc3910164d0a098ca78bbdb8981f
Kirill Skladanov <wra1th@inbox.ru>
2023-05-20 14:47:51 +0300

    <feat> изменить тип данных поля id, добавить поле для лайков

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -7,25 +7,29 @@ import lombok.Data;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class Film {
-    private int id;
+    private Long id;
     @NotBlank
     private final String name;
     @Size(max = 200, message = "максимальная длина описания — 200 символов")
     private final String description;
     private final LocalDate releaseDate;
     private final int duration;
+    private Set<Long> likes;
 
     @JsonCreator
-    public Film(@JsonProperty int id, @JsonProperty String name, @JsonProperty String description,
+    public Film(@JsonProperty Long id, @JsonProperty String name, @JsonProperty String description,
             @JsonProperty LocalDate releaseDate, @JsonProperty int duration) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.releaseDate = releaseDate;
         this.duration = duration;
+        this.likes = new HashSet<>();
     }
 
     @JsonCreator
