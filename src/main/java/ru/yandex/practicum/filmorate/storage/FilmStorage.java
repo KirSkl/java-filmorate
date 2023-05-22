201ad4a6d2a33e839ccc7e75390a408793759524
Kirill Skladanov <102518082+KirSkl@users.noreply.github.com>
2023-05-22 21:16:13 +0300

    Update FilmStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,8 +6,12 @@ import java.util.Collection;
 
 public interface FilmStorage {
     Collection<Film> findAllFilms();
+    
     Film addFilm(Film film);
+    
     void removeFilm(Long id);
+    
     Film updateFilm(Film film);
+    
     Film getFilmById(Long id);
 }
