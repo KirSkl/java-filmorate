8ab3df67c8ef4862f5159a8a2fa91f9dfbdda5d7
Kirill Skladanov <wra1th@inbox.ru>
2023-05-22 18:53:01 +0300

    <feat> добавить обработку исключений

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandlerController.java
@@ -0,0 +1,40 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.exception.ValidationIDException;
+
+import java.util.Map;
+
+@RestControllerAdvice
+@ResponseBody
+public class ErrorHandlerController {
+
+    @ExceptionHandler({UserNotFoundException.class, FilmNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFoundException(RuntimeException e) {
+        return Map.of("error:", "Не найдено", "errorMessage", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleNumberFormatException(NumberFormatException e) {
+        return Map.of("error", "ID должен быть указан в числовом формате",
+                "errorMessage", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationException(ValidationException e) {
+        return Map.of("error", "Не пройдена валидация", "errorMessage", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleValidationIDException(ValidationIDException e) {
+        return Map.of("error", "ID не существует", "errorMessage", e.getMessage());
+    }
+}
