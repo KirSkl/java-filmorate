6b5c6a70f7cfef4911af7d378c3b4afec8e7efea
Kirill Skladanov <102518082+KirSkl@users.noreply.github.com>
2023-05-22 21:20:51 +0300

    Update UserStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,8 +6,12 @@ import java.util.Collection;
 
 public interface UserStorage {
     Collection<User> findAllUsers();
+
     User addUser(User user);
+
     User updateUser(User user);
+
     void removeUser(Long id);
+
     User getUserById(Long id);
 }
