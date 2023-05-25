a84504b30823734f800da5c9acf7b39bc117c7fb
Kirill Skladanov <wra1th@inbox.ru>
2023-05-25 12:58:59 +0300

    <style> убрать ненужный импорт

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import lombok.Data;
-import lombok.Getter;
 import org.springframework.stereotype.Component;
 
 import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
