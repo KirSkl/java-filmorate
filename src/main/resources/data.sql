05e2b783e37da8bff60f2b0fd23719e9e557a9c4
Kirill Skladanov <wra1th@inbox.ru>
2023-06-08 22:17:51 +0300

    <feat> добавить данные по жанрам и рейтингам

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into MPA_RATING values ( 1,'G' );
+insert into MPA_RATING values ( 2,'PG' );
+insert into MPA_RATING values ( 3,'PG-13' );
+insert into MPA_RATING values ( 4,'R' );
+insert into MPA_RATING values ( 5,'NC-17' );
+
+INSERT INTO PUBLIC.GENRES (NAME) VALUES ('Комедия');
+INSERT INTO PUBLIC.GENRES (NAME) VALUES ('Драма');
+INSERT INTO PUBLIC.GENRES (NAME) VALUES ('Мультфильм');
+INSERT INTO PUBLIC.GENRES (NAME) VALUES ('Триллер');
+INSERT INTO PUBLIC.GENRES (NAME) VALUES ('Документальный');
+INSERT INTO PUBLIC.GENRES (NAME) VALUES ('Боевик');
