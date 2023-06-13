65ab6e88f6c8499d8672088f5f8a28d343619d6a
Kirill Skladanov <wra1th@inbox.ru>
2023-06-13 13:32:25 +0300

    <fix> убрать неизвестный мпа для тестов

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,9 @@
-insert into MPA_RATING values ( 1,'G' );
-insert into MPA_RATING values ( 2,'PG' );
-insert into MPA_RATING values ( 3,'PG-13' );
-insert into MPA_RATING values ( 4,'R' );
-insert into MPA_RATING values ( 5,'NC-17' );
+--insert into MPA_RATING values ( 0,'Not rated');
+insert into MPA_RATING values ( 1,'G');
+insert into MPA_RATING values ( 2,'PG');
+insert into MPA_RATING values ( 3,'PG-13');
+insert into MPA_RATING values ( 4,'R');
+insert into MPA_RATING values ( 5,'NC-17');
 
 INSERT INTO PUBLIC.GENRES (NAME) VALUES ('Комедия');
 INSERT INTO PUBLIC.GENRES (NAME) VALUES ('Драма');
