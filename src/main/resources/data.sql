9f25e6493a4cd3701b03142d19dcb446e28913e1
Kirill Skladanov <wra1th@inbox.ru>
2023-06-15 02:34:50 +0300

    <fix> исправления

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
+insert into MPA_RATING values ( 1, 'G');
+insert into MPA_RATING values ( 2, 'PG');
+insert into MPA_RATING values ( 3, 'PG-13');
+insert into MPA_RATING values ( 4, 'R');
+insert into MPA_RATING values ( 5, 'NC-17');
+
 insert into PUBLIC.GENRES (NAME) VALUES ('Комедия');
 insert into PUBLIC.GENRES (NAME) VALUES ('Драма');
 insert into PUBLIC.GENRES (NAME) VALUES ('Мультфильм');
