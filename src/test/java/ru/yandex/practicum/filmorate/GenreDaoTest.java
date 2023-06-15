cea262f3befd9830cf94b29cbca14c91d457c95e
Kirill Skladanov <wra1th@inbox.ru>
2023-06-15 15:44:06 +0300

    <fix> убрать создание списка в соответствующий тест

--- src/test/java/ru/yandex/practicum/filmorate/GenreDaoTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/GenreDaoTest.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -19,23 +18,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class GenreDaoTest {
-
-    private static final List<Genre> genres = new ArrayList<>();
+class GenreDaoTest {
     private final GenreDaoImpl genreDao;
 
-    @BeforeAll
-    static void loadInitialData() {
+    @Test
+    void shouldReturnAllGenres() {
+        List<Genre> genres = new ArrayList<>();
         genres.add(new Genre(1, "Комедия"));
         genres.add(new Genre(2, "Драма"));
         genres.add(new Genre(3, "Мультфильм"));
         genres.add(new Genre(4, "Триллер"));
         genres.add(new Genre(5, "Документальный"));
         genres.add(new Genre(6, "Боевик"));
-    }
 
-    @Test
-    void shouldReturnAllGenres() {
         assertEquals(genres, genreDao.getAllGenres());
     }
 
